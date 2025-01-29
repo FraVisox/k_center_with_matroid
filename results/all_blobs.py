cd18541a3bca6c2a713a5e50c0e0146bed1aebe0
Francesco Visona <visona.francesco@gmail.com>
2025-01-29 10:41:17 +0100

    Added doubling dimensions and blobs experiments

--- results/all_blobs.py
+++ results/all_blobs.py
@@ -5,10 +5,9 @@ import numpy as np
 
 
 #File to read from
-first = False
-type_of_graph = "doubling_dimension"
-file_name = "experiments_results/CHEN/"+type_of_graph+".csv"
-output_file = "graphs/CHEN_dimensionality"
+first = True
+file_name = "experiments_results/all_blobs.csv"
+output_file = "graphs/ALL_BLOBS_"
 
 #Parameters
 x_axis = "dimensions"
@@ -41,31 +40,18 @@ def read_and_plot(output_file_path):
         replace_dots_with_commas(file_name)
     df = pl.read_csv(source=file_name, separator=";")
     df = df.with_columns(
-        pl.lit("blobs").alias("dataset")
-    ).filter(
-        pl.col("algorithm").is_in(["CHEN", "CAPPDELTA05", "CAPPDELTA20", "PELLCAPPDELTA05", "PELLCAPPDELTA20"])
-    )
-    df = df.with_columns(
         pl.col("algorithm").str.replace("PELLCAPPDELTA05", "OursOblivious 0.5"),
     ).with_columns(
         pl.col("algorithm").str.replace("CAPPDELTA05", "Ours 0.5")
-    ).with_columns(
-        pl.col("algorithm").str.replace("PELLCAPPDELTA10", "OursOblivious 1.0"),
-    ).with_columns(
-        pl.col("algorithm").str.replace("CAPPDELTA10", "Ours 1.0")
-    ).with_columns(
-        pl.col("algorithm").str.replace("PELLCAPPDELTA15", "OursOblivious 1.5"),
-    ).with_columns(
-        pl.col("algorithm").str.replace("CAPPDELTA15", "Ours 1.5")
     ).with_columns(
         pl.col("algorithm").str.replace("PELLCAPPDELTA20", "OursOblivious 2.0"),
     ).with_columns(
         pl.col("algorithm").str.replace("CAPPDELTA20", "Ours 2.0")
     )
     for graph in y_axis:
-        g = sns.FacetGrid(df, col="dataset", sharex=False, sharey=False, aspect=1.5)
-        hue_order = ["CHEN", "OursOblivious 0.5", "Ours 0.5",# "OursOblivious 1.0", "Ours 1.0", "OursOblivious 1.5", "Ours 1.5", 
-                     "OursOblivious 2.0", "Ours 2.0"]
+        g = sns.FacetGrid(df, col="wsize", sharex=False, sharey=True, aspect=1.5)
+        hue_order = ["JONES","CHEN", "OursOblivious 0.5", "Ours 0.5",# "OursOblivious 1.0", "Ours 1.0", "OursOblivious 1.5", "Ours 1.5", 
+                    "OursOblivious 2.0", "Ours 2.0"]
         g.map_dataframe(
             sns.lineplot,  #barplot or lineplot
             x    = x_axis,   #x axis
@@ -84,6 +70,8 @@ def read_and_plot(output_file_path):
             )
         g.add_legend()
         #plt.gcf().set_size_inches(8, 5)
+        if graph == "query":
+            plt.yscale('log')
         plt.savefig(output_file_path+"_"+graph+".png", bbox_inches='tight')
 
 # USE
